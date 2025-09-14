6bcb210443cc4852a72725f237c7efc605480f18
snagatushar <nagatushars05@gmail.com>
2025-09-15 00:33:52 +0530

    finals3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import autoTable from "jspdf-autotable";
 
 // -------------------- Supabase --------------------
 const supabaseUrl = "https://begfjxlvjaubnizkvruw.supabase.co";
-const supabaseKey = "YOUR_SUPABASE_ANON_KEY"; // Replace with your Supabase anon key
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlZ2ZqeGx2amF1Ym5pemt2cnV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNjM0MzcsImV4cCI6MjA3MTYzOTQzN30.P6s1vWqAhXaNclfQw1NQ8Sj974uQJxAmoYG9mPvpKSQ"; // Replace with your Supabase anon key
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // -------------------- Helpers --------------------
